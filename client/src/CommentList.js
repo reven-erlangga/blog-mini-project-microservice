68b8ed46eabda1caccd8383ddab7419f59171b70
reven-erlangga <reven.erlangga@gmail.com>
2023-04-19 21:43:40 +0700

    add: adding feature moderation

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,8 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 export default function CommentList({ comments }) {
   const renderedComments = Object.values(comments).map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+
+    switch (comment.status) {
+      case "approved":
+        content = comment.content;
+        break;
+
+      case "rejected":
+        content = "This comment is rejected";
+        break;
+
+      default:
+        content = "This comment is awaiting moderation";
+        break;
+    }
+
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
--- comments/index.js
+++ comments/index.js
@@ -21,23 +21,53 @@ app.post("/posts/:id/comments", (req, res) => {
 
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: "pending" });
   commentsByPostId[req.params.id] = comments;
 
-  axios.post("http://localhost:4005/events", {
-    type: "CommentCreated",
-    data: {
-      id: commentId,
-      content,
-      postId: req.params.id,
-    },
-  });
+  axios
+    .post("http://localhost:4005/events", {
+      type: "CommentCreated",
+      data: {
+        id: commentId,
+        content,
+        postId: req.params.id,
+        status: "pending",
+      },
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 
   res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
+  console.log("comments");
   console.log("receive event : ", req.body.type);
+
+  const { type, data } = req.body;
+
+  if (type === "CommentModerated") {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+    comment.status = status;
+
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        status,
+        postId,
+        content,
+      },
+    });
+  }
+
+  res.send({});
 });
 
 app.listen(4001, () => {
